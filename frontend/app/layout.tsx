5b29b7f6e5a8ddcb171e697c73c80e9fef5589f8
chigozie0706 <chigoziejacob@gmail.com>
2026-04-25 20:26:03 +0100

    added toaster for notification

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Provider from "@/providers/WagmiProviders";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,10 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <Provider>{children}</Provider>
+        <Provider>
+          {children}
+          <Toaster position="top-center" richColors closeButton />
+        </Provider>
       </body>
     </html>
   );
