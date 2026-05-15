bcfbd23dae16d40b2af073630a6791231556b22e
chigozie0706 <chigoziejacob@gmail.com>
2026-05-15 02:47:57 +0100

    changed manifest information

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/.well-known/farcaster.json",
-        destination:
-          "https://api.farcaster.xyz/miniapps/hosted-manifest/019e2933-efa6-7e88-c4d1-eb17f4417093",
-        permanent: false, // Generates 307 temporary redirect
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
