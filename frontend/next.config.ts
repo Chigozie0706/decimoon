14e25e2532cd2546f688a8bff9d9a9513eee80db
chigozie0706 <chigoziejacob@gmail.com>
2026-05-15 02:32:25 +0100

    added redirects

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/.well-known/farcaster.json",
+        destination:
+          "https://api.farcaster.xyz/miniapps/hosted-manifest/019e2933-efa6-7e88-c4d1-eb17f4417093",
+        permanent: false, // Generates 307 temporary redirect
+      },
+    ];
+  },
 };
 
 export default nextConfig;
